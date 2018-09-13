0f0249c8fc04b67b531f6f819fa67bc920a4dd3f
nicejade <jeffygisgreat@gmail.com>
2018-09-13 22:30:45 +0800

    ✨ initial create & commit

---
+++ js/totop.js
@@ -0,0 +1,1 @@
+!function(o){var t=o("#totop");t.hide(),o(window).scroll(function(){o(document).scrollTop()>1e3?o(t).stop().fadeTo(300,1):o(t).stop().fadeTo(300,0)}),o(t).click(function(){return o("html, body").animate({scrollTop:0},500),!1})}(jQuery);
