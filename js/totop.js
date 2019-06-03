005fadfcefe2ceb1f4e332a99cdf9f6436c28144
nicejade <jeffygisgreat@gmail.com>
2019-06-03 08:38:19 +0800

    ✨ update blog article

--- js/totop.js
+++ js/totop.js
@@ -1,1 +1,1 @@
-!function(o){var t=o("#totop");t.hide(),o(window).scroll(function(){o(document).scrollTop()>1e3?o(t).stop().fadeTo(300,1):o(t).stop().fadeTo(300,0)}),o(t).click(function(){return o("html, body").animate({scrollTop:0},500),!1})}(jQuery);
+!function(o){var t=1e3,n=o("#totop"),c=500;n.hide(),o(window).scroll(function(){var c=o(document).scrollTop();c>t?o(n).stop().fadeTo(300,1):o(n).stop().fadeTo(300,0)}),o(n).click(function(){return o("html, body").animate({scrollTop:0},c),!1})}(jQuery);
